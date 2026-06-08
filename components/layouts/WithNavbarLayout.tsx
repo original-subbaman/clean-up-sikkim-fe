59fe3c116f1efa14c9e88140ad1ac8b6870bef6e
original-subbaman <abhisheksubba.py@gmail.com>
2026-06-08 08:51:33 +0530

    feat: add the map nav item back into the navbar

--- components/layouts/WithNavbarLayout.tsx
+++ components/layouts/WithNavbarLayout.tsx
@@ -2,8 +2,8 @@ import Navbar from "@/components/Navbar";
 import { type NavLinkItem } from "@/lib/routes";
 
 const mapLinks: NavLinkItem[] = [
-  // { path: "/map", label: "Map", isPrivate: false },
-  { path: "/community", label: "Community", isPrivate: true },
+  { path: "/map", label: "Map", isPrivate: false },
+  { path: "/community", label: "Community", isPrivate: false },
   { path: "/events/create", label: "Add Event", isPrivate: true },
   { path: "/events/my-events", label: "My Events", isPrivate: true },
 ];
